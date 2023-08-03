bb93b940eb3ce9566358cbd50d68c41240dc90f7
boyraf <raphael.kymotho@gmail.com>
2023-08-03 06:07:31 +0300

    re-wrote the routes for products

--- ecommerce/backend/app.py
+++ ecommerce/backend/app.py
@@ -1,28 +1,24 @@
 # server/app.py
-from flask import Flask, jsonify, request, make_response, session, redirect, url_for
-#from werkzeug.exceptions import HTTPException, NotFound
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from models import db 
+from models import db, User, EshopProductInfo, Comment, bcrypt
 from flask_cors import CORS
 from werkzeug.security import check_password_hash
-from models import db, User,  Comment, bcrypt
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-# app.json_encoder = SerializerMixin.json_encoder
-app.secret_key= b'm{\xf9\xec\xa0\xa7Gv\x98\x06\xa9\xfb\xdb\xe2\x8d\x86'
+app.secret_key = b'm{\xf9\xec\xa0\xa7Gv\x98\x06\xa9\xfb\xdb\xe2\x8d\x86'
 
 CORS(app)
 
 api = Api(app)
 db.init_app(app)
-# instantiate Bcrypt with app instance
 bcrypt.init_app(app)
 migrate = Migrate(app, db)
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -36,73 +32,74 @@ def login():
         return jsonify(user={"id": user.id, "username": user.username})
     else:
         return jsonify(message="Invalid email or password"), 401
-    
 
-# @app.route('/products', methods=['GET', 'POST'])
-# def get_products():
-#     if request.method == 'GET':    
-#         products = Product.query.order_by(Product.price.asc()).all()
-#         product_list = [product.to_dict() for product in products]
-#         response = make_response(jsonify(product_list), 200)
-#         return response
-#     elif request.method == 'POST':
-#         data=request.get_json()
 
-#         new_product= Product(
-#             name=data.get('name'),
-#             description=data.get('description'),
-#             image_url=data.get('image_url'),
-#             category=data.get('category'),
-#             stock=data.get('stock'),
-#             rating=0
-#         )
+@app.route('/products', methods=['GET', 'POST'])
+def get_products():
+    if request.method == 'GET':
+        products_info = EshopProductInfo.query.order_by(EshopProductInfo.price.asc()).all()
+        product_list = [product_info.to_dict() for product_info in products_info]
+        response = make_response(jsonify(product_list), 200)
+        return response
+    elif request.method == 'POST':
+        data = request.get_json()
 
-#         db.session.add(new_product)
-#         db.session.commit()
+        new_product = EshopProductInfo(
+            name=data.get('name'),
+            image_url=data.get('image_url'),
+            category=data.get('category'),
+            stock=data.get('stock'),
+            rating=0,
+            price=data.get('price'),
+            delivery_cost=data.get('delivery_cost')
+        )
 
-#         return make_response(
-#             jsonify({"message":"You have successfully added a new product"}),
-#             201
-#         ) 
-    
-# @app.route('/product/<int:product_id>', methods=['GET', 'PUT', 'DELETE'])
-# def product_operations(product_id):
-#     product = Product.query.get(product_id)
-#     if not product:
-#         return jsonify({'message': 'Product not found'}), 404
+        db.session.add(new_product)
+        db.session.commit()
 
-#     if request.method == 'GET':
-#         response_data = {
-#             'product': product.to_dict(),
-#             'comments': [comment.to_dict() for comment in product.comment]
-#         }
-#         return jsonify(response_data), 200
+        return make_response(
+            jsonify({"message": "You have successfully added a new product"}),
+            201
+        )
 
-#     if request.method == 'PUT':
-#         data = request.get_json()
-#         if not data:
-#             return jsonify({'message': 'Invalid data format'}), 400
 
-#         # Use data.get() with default values for optional fields
-#         product.name = data.get('name', product.name)
-#         product.image_url = data.get('image_url', product.image_url)
-#         product.description = data.get('description', product.description)
-#         product.category = data.get('category', product.category)
-#         product.stock = data.get('stock', product.stock)
-#         db.session.commit()
-#         return jsonify({'message': 'Product updated successfully'}), 200
+@app.route('/product/<int:product_id>', methods=['GET', 'PUT', 'DELETE'])
+def product_operations(product_id):
+    product_info = EshopProductInfo.query.get(product_id)
+    if not product_info:
+        return jsonify({'message': 'Product not found'}), 404
 
-#     if request.method == 'DELETE':
-#         db.session.delete(product)
-#         db.session.commit()
-#         return jsonify({'message': 'Product deleted successfully'}), 200
+    if request.method == 'GET':
+        response_data = {
+            'product': product_info.to_dict(),
+            'comments': [comment.to_dict() for comment in product_info.comments]
+        }
+        return jsonify(response_data), 200
 
-#     return jsonify({'message': 'Method not allowed'}), 405
+    if request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            return jsonify({'message': 'Invalid data format'}), 400
 
+        product_info.name = data.get('name', product_info.name)
+        product_info.image_url = data.get('image_url', product_info.image_url)
+        product_info.category = data.get('category', product_info.category)
+        product_info.stock = data.get('stock', product_info.stock)
+        product_info.price = data.get('price', product_info.price)
+        product_info.delivery_cost = data.get('delivery_cost', product_info.delivery_cost)
+
+        db.session.commit()
+        return jsonify({'message': 'Product updated successfully'}), 200
+
+    if request.method == 'DELETE':
+        db.session.delete(product_info)
+        db.session.commit()
+        return jsonify({'message': 'Product deleted successfully'}), 200
+
+    return jsonify({'message': 'Method not allowed'}), 405
 
 
 if __name__ == '__main__':
     with app.app_context():
-        db.init_app(app)
         db.create_all()
     app.run()
