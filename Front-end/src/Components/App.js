aa00797743df1c46dfedfc1c10dbd556aaafcb26
James <kamaujames3705@gmail.com>
2023-08-04 00:05:01 +0300

    Made specifications page

--- Front-end/src/Components/App.js
+++ Front-end/src/Components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css'; // Use './' to indicate the file path for importing CSS files
+// import SignUp from './Signup/SignUp';
+// import Cards from './cards/Cards';
+import Specs from './Specifications/Specs';
+
 const App = () => {
   return (
-    <div>      
+    <div>  
+      {/* <SignUp />   
+      <Cards />
+      <Footer /> */}
+      <Specs />
+
     </div>
   );
 };
