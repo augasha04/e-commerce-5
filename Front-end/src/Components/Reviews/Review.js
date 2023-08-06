ceb24b4845a59ab29150bbf49b70d51e63d6fdc5
James <kamaujames3705@gmail.com>
2023-08-06 20:26:00 +0300

    review component

--- Front-end/src/Components/Reviews/Review.js
+++ Front-end/src/Components/Reviews/Review.js
@@ -1,4 +1,72 @@
+// import React, { useState } from 'react';
+// import './review.css'
+
+// const Review = () => {
+//   const [reviews, setReviews] = useState([]);
+//   const [newReview, setNewReview] = useState({ name: '', comment: '' });
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setNewReview({ ...newReview, [name]: value });
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     if (newReview.name.trim() === '' || newReview.comment.trim() === '') {
+//       return; // Prevent submitting empty reviews
+//     }
+
+//     // Add the new review to the reviews list
+//     setReviews([...reviews, newReview]);
+
+//     // Clear the input fields
+//     setNewReview({ name: '', comment: '' });
+//   };
+
+//   return (
+//     <div>
+//       <h2>Reviews</h2>
+//       {reviews.length === 0 ? (
+//         <p>No reviews yet.</p>
+//       ) : (
+//         <ul>
+//           {reviews.map((review, index) => (
+//             <li key={index}>
+//               <strong>{review.name}:</strong> {review.comment}
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor="name">Name:</label>
+//           <input
+//             type="text"
+//             id="name"
+//             name="name"
+//             value={newReview.name}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor="comment">Comment:</label>
+//           <textarea
+//             id="comment"
+//             name="comment"
+//             value={newReview.comment}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <button type="submit">Submit Review</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Review;
+
 import React, { useState } from 'react';
+import './review.css'; // Import the CSS file
 
 const Review = () => {
   const [reviews, setReviews] = useState([]);
@@ -23,12 +91,12 @@ const Review = () => {
   };
 
   return (
-    <div>
-      <h2>Reviews</h2>
+    <div className="review-container">
+      <div className="review-title">Reviews</div>
       {reviews.length === 0 ? (
         <p>No reviews yet.</p>
       ) : (
-        <ul>
+        <ul className="review-list">
           {reviews.map((review, index) => (
             <li key={index}>
               <strong>{review.name}:</strong> {review.comment}
@@ -36,7 +104,7 @@ const Review = () => {
           ))}
         </ul>
       )}
-      <form onSubmit={handleSubmit}>
+      <form className="review-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
