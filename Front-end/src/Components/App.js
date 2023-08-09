121257e20b55b99c4bfe4a867164d4c24e9a4995
James <kamaujames3705@gmail.com>
2023-08-09 14:39:50 +0300

    Reviews component

--- Front-end/src/Components/App.js
+++ Front-end/src/Components/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css'; // Use './' to indicate the file path for importing CSS files
 // import SignUp from './Signup/SignUp';
 // import Cards from './cards/Cards';
-import Specs from './Specifications/Specs';
-import Review from './Reviews/Review';
+// import Specs from './Specifications/Specs';
+// import Review from './Reviews/Review';
 
 const App = () => {
   return (
     <div>  
-      <Review />
 
     </div>
   );
